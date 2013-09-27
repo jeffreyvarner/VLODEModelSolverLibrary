868d8d1f8df7453b53e5660b2e4eb6f7bbf32c29
jeffreyvarner <jeff@curveos.com>
2013-09-27 12:40:14 -0400

    Initial Commit

---
+++ VLODEModelSolverLibrary/VLODEModelSolverLibrary.h
@@ -0,0 +1,13 @@
+//
+//  VLODEModelSolverLibrary.h
+//  VLODEModelSolverLibrary
+//
+//  Created by Jeffrey Varner on 9/27/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VLODEModelSolverLibrary : NSObject
+
+@end
---
+++ VLODEModelSolverLibrary/VLODEModelSolverLibrary.m
@@ -0,0 +1,13 @@
+//
+//  VLODEModelSolverLibrary.m
+//  VLODEModelSolverLibrary
+//
+//  Created by Jeffrey Varner on 9/27/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import "VLODEModelSolverLibrary.h"
+
+@implementation VLODEModelSolverLibrary
+
+@end
